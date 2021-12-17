b8ba4ffd5e54cdb16c5486503faa73610a078e77
cibrax <cibrax@gmail.com>
2021-12-17 16:37:14 -0300

    initial version

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials';
+const nacl = require('tweetnacl');
+const bs58 = require('bs58');
+
+const nextAuthOptions = (req, res) => {
+    return {
+        providers: [
+            CredentialsProvider({
+                async authorize(credentials) {
+                    const nonce = req.cookies["auth-nonce"];
+
+                    const message = `Sign this message for authenticating with your wallet. Nonce: ${nonce}`;
+                    const messageBytes = new TextEncoder().encode(message);
+
+                    const publicKeyBytes = bs58.decode(credentials.publicKey);
+                    const signatureBytes = bs58.decode(credentials.signature);
+                    
+                    const result = nacl.sign.detached.verify(messageBytes, signatureBytes, publicKeyBytes);
+
+                    if (!result) {
+                        console.log(`authentication failed`);
+                        throw new Error("user can not be authenticated");
+                    }
+
+                    const user = { name: credentials.publicKey }
+
+                    return user;
+                }
+            })
+        ]
+    }
+}
+
+export default (req, res) => {
+    return NextAuth(req, res, nextAuthOptions(req, res))
+}
---
+++ pages/api/login.js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import crypto from 'crypto';
+import { serialize } from 'cookie';
+
+export default function handler(req, res) {
+  if(req.method === "GET") {
+    const nonce = crypto.randomBytes(32).toString('base64');
+
+    res.setHeader('Set-Cookie', serialize('auth-nonce', nonce, { httpOnly: true, sameSite: 'strict', secure: true }));
+
+    return res.status(200).json({nonce});
+
+  }
+
+  res.status(405);
+}
